22e10d8a4e672dd1db02b0bbe79abea8bcbc4f98
sist <sist@localhost>
2018-12-17 16:47:02 +0900

    asd

---
+++ src/main/java/com/team/movie/MainContoller.java
@@ -0,0 +1,13 @@
+package com.team.movie;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainContoller {
+   @RequestMapping("main.do")
+   public String main_page()
+   {
+	   return "main";
+   }
+}
